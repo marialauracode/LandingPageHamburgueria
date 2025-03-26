d71225207f354e5452688685fa3a137d1979fce2
Maria Laura Castro <cmarialaura599@gmail.com>
2025-03-26 09:45:28 -0300

    Update index.html

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Selecionando os elementos do menu e navegação
+// Pega o menu e os itens de navegação, conforme classificamos no html
 const menu = document.querySelector('.menu')
 const navMenu = document.querySelector('.nav-menu')
 
 // Adicionando evento para alternar o estado do menu
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo') // Alterna a classe 'ativo' no menu
-    navMenu.classList.toggle('ativo') // Alterna a classe 'ativo' na lista de navegação
+menu.addEventListener('click', () => { // quando o usuário clicar no menu...
+
+    // toggle ativa ou remove a classe 'ativo', que está personalizada no CSS
+    // clicou uma vez = aparece
+    // clicou de novo = some
+    menu.classList.toggle('ativo')  // tanto para o menu...
+    navMenu.classList.toggle('ativo') //... quanto para os itens de navegação
 })
